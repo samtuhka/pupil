478ba1e67e1d6aa6743474ff0da7675181cfdd60
Samuel Tuhkanen <samtuhka@localhost.localdomain>
2015-07-20 12:04:29 +0300

    change market locations

--- pupil_src/shared_modules/calibration_routines/accuracy_test.py
+++ pupil_src/shared_modules/calibration_routines/accuracy_test.py
@@ -131,7 +131,7 @@ class Accuracy_Test(Screen_Marker_Calibration,Calibration_Plugin):
         self.sites = [  (.5, .5), (0,.5),
                         (0.,1),(.5,1),(1.,1.),
                         (1,.5),
-                        (1., 0),(.5, 0),(0,0.),
+                        (1., 0),(.5, .25),(0,0.),
                         (.5,.5),(.5,.5)]
         self.sites = np.random.random((10,2)).tolist() + self.sites
         self.active_site = 0
