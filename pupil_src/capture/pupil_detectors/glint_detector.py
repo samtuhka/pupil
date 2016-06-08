7033aec8a92e6e38e79f481fbf756f188b1d06de
Samuel Tuhkanen <samtuhka@localhost.localdomain>
2016-06-08 14:27:01 +0300

    remove useless copy-paste

--- pupil_src/capture/pupil_detectors/glint_detector.py
+++ pupil_src/capture/pupil_detectors/glint_detector.py
@@ -26,62 +26,6 @@ from pyglui import ui
 import logging
 logger = logging.getLogger(__name__)
 
-class Roi(object):
-    """this is a simple 2D Region of Interest class
-    it is applied on numpy arrays for convenient slicing
-    like this:
-    roi_array_slice = full_array[r.view]
-    # do something with roi_array_slice
-    this creates a view, no data copying done
-    """
-    def __init__(self, array_shape):
-        self.array_shape = array_shape
-        self.lX = 0
-        self.lY = 0
-        self.uX = array_shape[1]
-        self.uY = array_shape[0]
-        self.nX = 0
-        self.nY = 0
-
-    @property
-    def view(self):
-        return slice(self.lY,self.uY,),slice(self.lX,self.uX)
-
-    @view.setter
-    def view(self, value):
-        raise Exception('The view field is read-only. Use the set methods instead')
-
-    def add_vector(self,(x,y)):
-        """
-        adds the roi offset to a len2 vector
-        """
-        return (self.lX+x,self.lY+y)
-
-    def sub_vector(self,(x,y)):
-        """
-        subs the roi offset to a len2 vector
-        """
-        return (x-self.lX,y-self.lY)
-
-    def set(self,vals):
-        if vals is not None and len(vals) is 5:
-            if vals[-1] == self.array_shape:
-                self.lX,self.lY,self.uX,self.uY,_ = vals
-            else:
-                logger.info('Image size has changed: Region of Interest has been reset')
-        elif vals is not None and len(vals) is 4:
-            self.lX,self.lY,self.uX,self.uY= vals
-
-    def get(self):
-        return self.lX,self.lY,self.uX,self.uY,self.array_shape
-
-
-
-def bin_thresholding(image, image_lower=0, image_upper=256):
-    binary_img = cv2.inRange(image, np.asarray(image_lower),
-                np.asarray(image_upper))
-
-    return binary_img
 
 class Glint_Detector(object):
 
@@ -219,9 +163,8 @@ class Glint_Detector(object):
             cv2.circle(img,(30,30), r_min,(0,255,0),1)
             cv2.circle(img,(30,30), r_max,(0,0,255),1)
 
-
             overlay =  img[u_roi.view][roi.view]
-            overlay[::2,0] = 255 #yeay numpy broadcasting
+            overlay[::2,0] = 255
             overlay[::2,-1]= 255
             overlay[0,::2] = 255
             overlay[-1,::2]= 255
