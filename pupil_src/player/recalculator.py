a119da8512f6125d450af78bd9c4c77155d3153e
Samuel Tuhkanen <samuel.tuhkanen@helsinki.fi>
2017-07-31 13:42:44 +0300

    just import main for less copy-paste

--- pupil_src/player/recalculator.py
+++ pupil_src/player/recalculator.py
@@ -1,54 +1,9 @@
-import sys
-import os
-import platform
-
-if getattr(sys, 'frozen', False):
-    user_dir = os.path.expanduser(os.path.join('~', 'pupil_player_settings'))
-    version_file = os.path.join(sys._MEIPASS, '_version_string_')
-else:
-    # We are running in a normal Python environment.
-    # Make all pupil shared_modules available to this Python session.
-    pupil_base_dir = os.path.abspath(__file__).rsplit('pupil_src', 1)[0]
-    sys.path.append(os.path.join(pupil_base_dir, 'pupil_src', 'shared_modules'))
-    sys.path.append(os.path.join(pupil_base_dir, 'pupil_src', 'capture'))
-
-    # Specifiy user dirs.
-    user_dir = os.path.join(pupil_base_dir, 'player_settings')
-    version_file = None
-
-import logging
-# set up root logger before other imports
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-
-# since we are not using OS.fork on MacOS we need to do a few extra things to log our exports correctly.
-if platform.system() == 'Darwin':
-    if __name__ == '__main__':  # clear log if main
-        fh = logging.FileHandler(os.path.join(user_dir, 'recalc.log'), mode='w')
-    # we will use append mode since the exporter will stream into the same file when using os.span processes
-    fh = logging.FileHandler(os.path.join(user_dir, 'recalc.log'), mode='a')
-else:
-    fh = logging.FileHandler(os.path.join(user_dir, 'recalc.log'), mode='w')
-fh.setLevel(logging.DEBUG)
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-# create formatter and add it to the handlers
-formatter = logging.Formatter('Player: %(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-formatter = logging.Formatter('Player [%(levelname)s] %(name)s : %(message)s')
-ch.setFormatter(formatter)
-# add the handlers to the logger
-logger.addHandler(fh)
-logger.addHandler(ch)
-
-logging.getLogger("OpenGL").setLevel(logging.ERROR)
-logger = logging.getLogger(__name__)
-
+import main
 import numpy as np
 from time import sleep
 from vis_eye_video_overlay import Vis_Eye_Video_Overlay
 import threading
+import os, sys
 
 class Global_Container(object):
     pass
